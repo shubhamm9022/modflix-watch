9ecd1b5c4a72a5eaca3f1627a7a040f777432ea3
shubhamm9022 <shubhamwaghmode006@gmail.com>
2025-10-04 23:25:46 +0530

    Create slug-generator.js

---
+++ utils/slug-generator.js
@@ -0,0 +1,10 @@
+function generateSlug(length = 8) {
+  const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
+module.exports = { generateSlug };
